7ea3b6ca03a8526c7cd6a7d018a761aab2656da1
YuXiang187 <59519298+YuXiang187@users.noreply.github.com>
2023-03-16 06:12:32 +0800

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,152 @@
+import com.github.kwhat.jnativehook.GlobalScreen;
+import com.github.kwhat.jnativehook.NativeHookException;
+import com.github.kwhat.jnativehook.keyboard.NativeKeyEvent;
+import com.github.kwhat.jnativehook.keyboard.NativeKeyListener;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.util.Objects;
+import java.util.Random;
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class Main extends JDialog implements NativeKeyListener {
+    boolean isRun = false;
+    boolean isShow = false;
+
+    JLabel mainLabel;
+    Thread thread;
+    Timer timer;
+    TrayIcon trayIcon;
+
+    public void nativeKeyPressed(NativeKeyEvent e) {
+        if (e.getKeyCode() == 29) {
+            run();
+        }
+    }
+
+    public Main() {
+        setTitle("YuXiang Drawer");
+        setSize(450, 250);
+        setIconImage(new ImageIcon(Objects.requireNonNull(this.getClass().getResource("/icon/run.png"))).getImage());
+        setLocationRelativeTo(null);
+        setAlwaysOnTop(true);
+        setVisible(false);
+        setDefaultCloseOperation(DO_NOTHING_ON_CLOSE);
+        addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent e) {
+                if (!isRun) {
+                    setVisible(false);
+                }
+            }
+        });
+
+        trayIcon = new TrayIcon(new ImageIcon(Objects.requireNonNull(this.getClass().getResource("/trayicon/trayrun.png"))).getImage());
+
+        JPanel mainPanel = new JPanel();
+        mainPanel.setLayout(new BorderLayout());
+
+        mainLabel = new JLabel("未运行");
+        mainLabel.setFont(new Font("微软雅黑", Font.BOLD, 80));
+        mainLabel.setHorizontalAlignment(SwingConstants.CENTER);
+
+        mainPanel.add(mainLabel, BorderLayout.CENTER);
+        setContentPane(mainPanel);
+        validate();
+
+        if (SystemTray.isSupported()) {
+            trayIcon.setToolTip("YuXiang Drawer");
+
+            PopupMenu popupMenu = new PopupMenu();
+
+            MenuItem runItem = new MenuItem("运行(Ctrl)");
+            runItem.addActionListener(e -> run());
+            popupMenu.add(runItem);
+
+            MenuItem exitItem = new MenuItem("退出");
+            exitItem.addActionListener(e -> System.exit(0));
+            popupMenu.add(exitItem);
+
+            trayIcon.setPopupMenu(popupMenu);
+            SystemTray systemTray = SystemTray.getSystemTray();
+            try {
+                systemTray.add(trayIcon);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        } else {
+            JOptionPane.showMessageDialog(null, "您当前的操作系统不支持系统托盘。");
+            System.exit(0);
+        }
+    }
+
+    private void run() {
+        if (!isShow) {
+            isShow = true;
+            setVisible(true);
+            if (isRun) {
+                stop();
+            } else {
+                start();
+            }
+            timer = new Timer();
+            timer.schedule(new TimerTask() {
+                @Override
+                public void run() {
+                    if (isRun) {
+                        stop();
+                    } else {
+                        start();
+                    }
+                    isShow = false;
+                    timer.cancel();
+                }
+            }, 1500);
+        }
+    }
+
+    private void start() {
+        mainLabel.setForeground(Color.GRAY);
+        trayIcon.setImage(new ImageIcon(Objects.requireNonNull(this.getClass().getResource("/trayicon/traystop.png"))).getImage());
+        setIconImage(new ImageIcon(Objects.requireNonNull(this.getClass().getResource("/icon/stop.png"))).getImage());
+        isRun = true;
+        thread = new Thread(() -> {
+            Random random = new Random();
+            String[] items = {"Item1", "Item2", "Item3"};
+            int index;
+            while (isRun) {
+                index = random.nextInt(items.length);
+                mainLabel.setText(items[index]);
+                try {
+                    Thread.sleep(100);
+                } catch (InterruptedException e) {
+                    break;
+                }
+            }
+        });
+        thread.start();
+    }
+
+    private void stop() {
+        mainLabel.setForeground(Color.BLACK);
+        trayIcon.setImage(new ImageIcon(Objects.requireNonNull(this.getClass().getResource("/trayicon/trayrun.png"))).getImage());
+        setIconImage(new ImageIcon(Objects.requireNonNull(this.getClass().getResource("/icon/run.png"))).getImage());
+        isRun = false;
+        if (thread != null) {
+            thread.interrupt();
+            thread = null;
+        }
+    }
+
+    public static void main(String[] args) {
+        try {
+            GlobalScreen.registerNativeHook();
+        } catch (NativeHookException ex) {
+            JOptionPane.showMessageDialog(null, "系统按键注册失败。");
+            System.exit(0);
+        }
+        GlobalScreen.addNativeKeyListener(new Main());
+    }
+}
