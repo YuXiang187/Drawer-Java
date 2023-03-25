ec76f4856c302f0053fe4a3b566bd81a14b36f0c
YuXiang187 <59519298+YuXiang187@users.noreply.github.com>
2023-03-25 22:15:56 +0800

    Create EncryptString.java

---
+++ EncryptString.java
@@ -0,0 +1,47 @@
+import java.security.Key;
+import javax.crypto.Cipher;
+
+public class EncryptString {
+    private static final String strDefaultKey = "yuxiang2023@#$%^&";
+
+    private final Cipher decryptCipher;
+
+    public EncryptString() throws Exception {
+        this(strDefaultKey);
+    }
+
+    public EncryptString(String strKey) throws Exception {
+        Key key = getKey(strKey.getBytes());
+        Cipher encryptCipher = Cipher.getInstance("DES");
+        encryptCipher.init(Cipher.ENCRYPT_MODE, key);
+        decryptCipher = Cipher.getInstance("DES");
+        decryptCipher.init(Cipher.DECRYPT_MODE, key);
+    }
+
+    public static byte[] hexStr2ByteArr(String strIn) {
+        byte[] arrB = strIn.getBytes();
+        int iLen = arrB.length;
+        byte[] arrOut = new byte[iLen / 2];
+        for (int i = 0; i < iLen; i = i + 2) {
+            String strTmp = new String(arrB, i, 2);
+            arrOut[i / 2] = (byte) Integer.parseInt(strTmp, 16);
+        }
+        return arrOut;
+    }
+
+    public byte[] decrypt(byte[] arrB) throws Exception {
+        return decryptCipher.doFinal(arrB);
+    }
+
+    public String decrypt(String strIn) throws Exception {
+        return new String(decrypt(hexStr2ByteArr(strIn)));
+    }
+
+    private Key getKey(byte[] arrBTmp) {
+        byte[] arrB = new byte[8];
+        for (int i = 0; i < arrBTmp.length && i < arrB.length; i++) {
+            arrB[i] = arrBTmp[i];
+        }
+        return new javax.crypto.spec.SecretKeySpec(arrB, "DES");
+    }
+}
