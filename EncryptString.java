e9d01d51fb7187472e6d9a0a87eeda7378b7db90
YuXiang187 <59519298+YuXiang187@users.noreply.github.com>
2023-04-02 08:00:26 +0800

    Update EncryptString.java

--- EncryptString.java
+++ EncryptString.java
@@ -2,14 +2,24 @@ import java.security.Key;
 import javax.crypto.Cipher;
 
 public class EncryptString {
+    // 字符串默认键值
     private static final String strDefaultKey = "yuxiang2023@#$%^&";
 
+    // 解密工具
     private final Cipher decryptCipher;
 
+    /**
+     * 默认构造方法，使用默认密钥
+     */
     public EncryptString() throws Exception {
         this(strDefaultKey);
     }
 
+    /**
+     * 指定密钥构造方法
+     *
+     * @param strKey 指定的密钥
+     */
     public EncryptString(String strKey) throws Exception {
         Key key = getKey(strKey.getBytes());
         Cipher encryptCipher = Cipher.getInstance("DES");
@@ -18,6 +28,13 @@ public class EncryptString {
         decryptCipher.init(Cipher.DECRYPT_MODE, key);
     }
 
+    /**
+     * 将表示16进制值的字符串转换为byte数组，和public static String byteArr2HexStr(byte[] arrB)
+     * 互为可逆的转换过程
+     *
+     * @param strIn 需要转换的字符串
+     * @return 转换后的byte数组
+     */
     public static byte[] hexStr2ByteArr(String strIn) {
         byte[] arrB = strIn.getBytes();
         int iLen = arrB.length;
@@ -29,14 +46,32 @@ public class EncryptString {
         return arrOut;
     }
 
+    /**
+     * 解密字节数组
+     *
+     * @param arrB 需解密的字节数组
+     * @return 解密后的字节数组
+     */
     public byte[] decrypt(byte[] arrB) throws Exception {
         return decryptCipher.doFinal(arrB);
     }
 
+    /**
+     * 解密字符串
+     *
+     * @param strIn 需解密的字符串
+     * @return 解密后的字符串
+     */
     public String decrypt(String strIn) throws Exception {
         return new String(decrypt(hexStr2ByteArr(strIn)));
     }
 
+    /**
+     * 从指定字符串生成密钥，密钥所需的字节数组长度为8位 不足8位时后面补0，超出8位只取前8位
+     *
+     * @param arrBTmp 构成该字符串的字节数组
+     * @return 生成的密钥
+     */
     private Key getKey(byte[] arrBTmp) {
         byte[] arrB = new byte[8];
         for (int i = 0; i < arrBTmp.length && i < arrB.length; i++) {
