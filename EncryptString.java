8ac84f5c1b0fb4bc6c77d60fc5a5c92bc2477d75
YuXiang187 <59519298+YuXiang187@users.noreply.github.com>
2023-04-15 14:48:40 +0800

    Update EncryptString.java

--- EncryptString.java
+++ EncryptString.java
@@ -2,8 +2,12 @@ import java.security.Key;
 import javax.crypto.Cipher;
 
 public class EncryptString {
+
     // 字符串默认键值
     private static final String strDefaultKey = "yuxiang2023@#$%^&";
+
+    // 加密工具
+    private final Cipher encryptCipher;
 
     // 解密工具
     private final Cipher decryptCipher;
@@ -22,10 +26,37 @@ public class EncryptString {
      */
     public EncryptString(String strKey) throws Exception {
         Key key = getKey(strKey.getBytes());
-        Cipher encryptCipher = Cipher.getInstance("DES");
+        encryptCipher = Cipher.getInstance("DES");
         encryptCipher.init(Cipher.ENCRYPT_MODE, key);
         decryptCipher = Cipher.getInstance("DES");
         decryptCipher.init(Cipher.DECRYPT_MODE, key);
+    }
+
+    /**
+     * 将byte数组转换为表示16进制值的字符串， 如：byte[]{8,18}转换为：0813，和public static byte[]
+     * <p>
+     * hexStr2ByteArr(String strIn) 互为可逆的转换过程
+     *
+     * @param arrB 需要转换的byte数组
+     * @return 转换后的字符串
+     */
+    public static String byteArr2HexStr(byte[] arrB) {
+        int iLen = arrB.length;
+        // 每个byte用2个字符才能表示，所以字符串的长度是数组长度的2倍
+        StringBuilder sb = new StringBuilder(iLen * 2);
+        for (int b : arrB) {
+            int intTmp = b;
+            // 把负数转换为正数
+            while (intTmp < 0) {
+                intTmp = intTmp + 256;
+            }
+            // 小于0F的数需要在前面补0
+            if (intTmp < 16) {
+                sb.append("0");
+            }
+            sb.append(Integer.toString(intTmp, 16));
+        }
+        return sb.toString();
     }
 
     /**
@@ -38,12 +69,33 @@ public class EncryptString {
     public static byte[] hexStr2ByteArr(String strIn) {
         byte[] arrB = strIn.getBytes();
         int iLen = arrB.length;
+        // 两个字符表示一个字节，所以字节数组长度是字符串长度除以2
         byte[] arrOut = new byte[iLen / 2];
         for (int i = 0; i < iLen; i = i + 2) {
             String strTmp = new String(arrB, i, 2);
             arrOut[i / 2] = (byte) Integer.parseInt(strTmp, 16);
         }
         return arrOut;
+    }
+
+    /**
+     * 加密字节数组
+     *
+     * @param arrB 需加密的字节数组
+     * @return 加密后的字节数组
+     */
+    public byte[] encrypt(byte[] arrB) throws Exception {
+        return encryptCipher.doFinal(arrB);
+    }
+
+    /**
+     * 加密字符串
+     *
+     * @param strIn 需加密的字符串
+     * @return 加密后的字符串
+     */
+    public String encrypt(String strIn) throws Exception {
+        return byteArr2HexStr(encrypt(strIn.getBytes()));
     }
 
     /**
@@ -73,7 +125,9 @@ public class EncryptString {
      * @return 生成的密钥
      */
     private Key getKey(byte[] arrBTmp) {
+        // 创建一个空的8位字节数组（默认值为0）
         byte[] arrB = new byte[8];
+        // 将原始字节数组转换为8位
         for (int i = 0; i < arrBTmp.length && i < arrB.length; i++) {
             arrB[i] = arrBTmp[i];
         }
