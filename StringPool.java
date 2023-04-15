21259e7dba8cab85908b9cb9d1f01df211ad7750
YuXiang187 <59519298+YuXiang187@users.noreply.github.com>
2023-04-15 17:12:39 +0800

    Update StringPool.java

--- StringPool.java
+++ StringPool.java
@@ -23,7 +23,7 @@ public class StringPool {
             JOptionPane.showMessageDialog(null, "list.es或pool.es文件读取错误，无法启动本程序。", "YuXiang Drawer", JOptionPane.ERROR_MESSAGE);
             System.exit(0);
         }
-        if (pool.size() == 0) {
+        if (pool.get(0).equals("")) {
             pool = new ArrayList<>(initPool);
         }
         Timer timer = new Timer();
