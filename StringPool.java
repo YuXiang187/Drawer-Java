b8120b461f043510caa9cc15512e0964d10d7ffd
YuXiang187 <59519298+YuXiang187@users.noreply.github.com>
2023-07-01 14:30:04 +0800

    Update StringPool.java

--- StringPool.java
+++ StringPool.java
@@ -9,19 +9,16 @@ public class StringPool {
     EncryptString es;
     List<String> initPool;
     List<String> pool;
+    Random random;
 
     public StringPool() {
-        try {
-            es = new EncryptString();
-            initPool = new ArrayList<>(Arrays.asList(es.decrypt(readFile("list.es")).split(",")));
-            if (Files.exists(Paths.get("pool.es"))) {
-                pool = new ArrayList<>(Arrays.asList(es.decrypt(readFile("pool.es")).split(",")));
-            } else {
-                reset();
-            }
-        } catch (Exception e) {
-            JOptionPane.showMessageDialog(null, "list.es或pool.es文件读取错误，无法启动本程序。", "YuXiang Drawer", JOptionPane.ERROR_MESSAGE);
-            System.exit(0);
+        es = new EncryptString();
+        random = new Random();
+        initPool = new ArrayList<>(Arrays.asList(readFile("list.es").split(",")));
+        if (Files.exists(Paths.get("pool.es"))) {
+            pool = new ArrayList<>(Arrays.asList(readFile("pool.es").split(",")));
+        } else {
+            reset();
         }
         if (pool.get(0).equals("")) {
             pool = new ArrayList<>(initPool);
@@ -32,39 +29,42 @@ public class StringPool {
             public void run() {
                 saveFile();
             }
-        }, 0, 600000);
+        }, 0, 600000); // 每10分钟保存一次
     }
 
-    public String get(int index) {
-        return pool.get(index);
+    // 获取随机字符串
+    protected String getRandomString() {
+        if (pool.size() == 0) {
+            reset();
+        }
+        return pool.get(random.nextInt(pool.size()));
     }
 
-    public void remove(int index) {
-        pool.remove(index);
-    }
-
-    public int length() {
-        return pool.size();
-    }
-
-    public void reset() {
+    // 重置卡池
+    protected void reset() {
         pool = new ArrayList<>(initPool);
     }
 
-    private String readFile(String fileName) {
-        StringBuilder contentBuilder = new StringBuilder();
-        try (BufferedReader reader = new BufferedReader(new FileReader(fileName))) {
-            String line;
-            while ((line = reader.readLine()) != null) {
-                contentBuilder.append(line);
-            }
-        } catch (IOException e) {
-            JOptionPane.showMessageDialog(null, "list.es或pool.es文件读取错误，无法启动本程序。", "YuXiang Drawer", JOptionPane.ERROR_MESSAGE);
-            System.exit(0);
-        }
-        return contentBuilder.toString();
+    // 删除字符串
+    protected void remove(String str) {
+        pool.remove(str);
     }
 
+    // 读取文件
+    private String readFile(String fileName) {
+        StringBuilder contentBuilder = new StringBuilder();
+        String text = null;
+        try (BufferedReader reader = new BufferedReader(new FileReader(fileName))) {
+            contentBuilder.append(reader.readLine());
+            text = es.decrypt(contentBuilder.toString());
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, fileName + "文件读取错误，无法启动本程序。", "YuXiang Drawer", JOptionPane.ERROR_MESSAGE);
+            System.exit(0);
+        }
+        return text;
+    }
+
+    // 保存文件
     public void saveFile() {
         try (BufferedWriter writer = new BufferedWriter(new FileWriter("pool.es"))) {
             writer.write(es.encrypt(String.join(",", pool)));
